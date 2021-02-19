cbb80433c79de1b1371e24ea46bd7a77a5f80cff
mgarridov <mgarridov@minsait.com>
2021-02-19 14:23:09 +0100

    feat(usuario): create new user

--- controllers/usuarioContoller.js
+++ controllers/usuarioContoller.js
@@ -1,16 +1,51 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario = async (req, res) => {
+  // Revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  const { email, password } = req.body;
   try {
-    let usuario;
+    // Revisar que el usuario sea único
+
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({ msg: 'El usuario ya existe' });
+    }
 
     // Crea el usuario
     usuario = new Usuario(req.body);
-    res.send('Usuario creado correctamente');
+
+    // Hashear el password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
+
     // Guarda el usuario
     await usuario.save();
+
+    // Crear y firmar el JWT
+    const payload = {
+      usuario: {
+        id: usuario.id,
+      },
+    };
+
+    // Firmar JWT
+    jwt.sign(payload, process.env.SECRET, { expiresIn: 3600 }, (error, token) => {
+      if (error) throw error;
+
+      // Mensaje de confirmación
+      res.json({ token });
+    });
   } catch (error) {
     console.log(error);
-    res.status(400).send('Hubo un error al crear el usuario');
+    res.status(400).json({ msg: 'Hubo un error al crear el usuario' });
   }
 };
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioContoller');
+const { check } = require('express-validator');
 
 // Crea un usuario
 // api/usuarios
-router.post('/', usuarioController.crearUsuario);
+router.post(
+  '/',
+  [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 }),
+  ],
+  usuarioController.crearUsuario,
+);
 
 module.exports = router;

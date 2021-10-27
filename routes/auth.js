1cb016be6d5ea9bb5f50e4bb3a7d79e369cfeef5
mgarridov <mgarridov@minsait.com>
2021-10-27 19:07:34 +0200

    feat(auth): validation removed

--- routes/auth.js
+++ routes/auth.js
@@ -6,14 +6,7 @@ const auth = require('../middleware/auth');
 
 // Crea un usuario
 // api/usuarios
-router.post(
-  '/',
-  [
-    check('email', 'El email es obligatorio').not().isEmpty(),
-    check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 }),
-  ],
-  authController.autenticarUsuario,
-);
+router.post('/', authController.autenticarUsuario);
 
 router.get('/', auth, authController.usuarioAutenticado);
 

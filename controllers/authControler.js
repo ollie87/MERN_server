4515a2d26ad419539feba6c01fc61ec4f328eed9
mgarridov <mgarridov@minsait.com>
2021-10-27 18:40:37 +0200

    feat(auth): check token

--- controllers/authControler.js
+++ controllers/authControler.js
@@ -45,3 +45,13 @@ exports.autenticarUsuario = async (req, res) => {
     res.status(400).json({ msg: 'Hubo un error al hacer login' });
   }
 };
+
+exports.usuarioAutenticado = async (req, res) => {
+  try {
+    const usuario = await Usuario.findById(req.usuario.id).select('-password');
+    res.json({ usuario });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: 'Hubo un error' });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authControler');
 const { check } = require('express-validator');
+const auth = require('../middleware/auth');
 
 // Crea un usuario
 // api/usuarios
@@ -13,5 +14,7 @@ router.post(
   ],
   authController.autenticarUsuario,
 );
+
+router.get('/', auth, authController.usuarioAutenticado);
 
 module.exports = router;
